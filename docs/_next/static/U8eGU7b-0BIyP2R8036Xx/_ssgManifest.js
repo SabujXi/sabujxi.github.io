2c7d1c394630d0b8c3fcd9109e011f6242dbb012
Md. Sabuj Sarker <md.sabuj.sarker@gmail.com>
2022-06-11 23:01:52 +0600

    exported site after updates

---
+++ docs/_next/static/U8eGU7b-0BIyP2R8036Xx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fservices","\u002Ffor-hire","\u002F","\u002Fprojects","\u002Fservices\u002F[slug]","\u002Ffor-hire\u002F[slug]","\u002Fprojects\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
